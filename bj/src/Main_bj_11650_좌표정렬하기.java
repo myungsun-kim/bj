4f33e7d1d3403a65cca2283eabdef7c381221ef6
kms94 <kms94@192.168.1.164>
2021-03-19 19:18:20 +0900

    백준 좌표 정렬하기 1,2

---
+++ bj/src/Main_bj_11650_좌표정렬하기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_11650_좌표정렬하기 {
+	private static class Node implements Comparable<Node>{
+		int x;
+		int y;
+		
+		public Node(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			int dif = Integer.compare(this.x, o.x);
+			if(dif==0) {
+				return Integer.compare(this.y, o.y);
+			}else{
+				return dif;
+			}
+		}
+		
+	}
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_11650.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		Node[] arr = new Node[n];
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			arr[i] = new Node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+		}
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++) {
+			sb.append(arr[i].x).append(" ").append(arr[i].y).append("\n");
+		}
+		br.close();
+		sb.setLength(sb.length()-1);
+		System.out.print(sb);
+	}
+}
