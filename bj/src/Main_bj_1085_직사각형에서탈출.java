114ca7f76cf04f1f56cd288baac305fd5f283e75
kms94 <kms94@192.168.1.40>
2021-03-26 16:35:56 +0900

    백준 직사각형에서의 탈출

---
+++ bj/src/Main_bj_1085_직사각형에서탈출.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_1085_직사각형에서탈출 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_1085.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int x = Integer.parseInt(st.nextToken());//현 위치 x
+		int y = Integer.parseInt(st.nextToken());//현 위치 y
+		int w = Integer.parseInt(st.nextToken());//오른쪽 위 꼭지점 x
+		int h = Integer.parseInt(st.nextToken());//오른쪽 위 꼭지점 y
+		br.close();
+		System.out.print(Math.min(Math.min(Math.abs(x-w), x), Math.min(Math.abs(y-h),y)));
+	}
+}
