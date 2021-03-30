98adb4f467e6a6f1eb68a8533dcc363cf4865fc6
kms94 <kms94@192.168.1.40>
2021-03-31 00:06:48 +0900

    백준 4949, 9012, 10773

---
+++ bj/src/Main_bj_10773_제로.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_10773_제로 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10773.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int input;
+		int sum=0;
+		Stack<Integer> s = new Stack<>();
+		for(int i=0;i<N;i++) {
+			input = Integer.parseInt(br.readLine());
+			if(input==0) {
+				sum-=s.pop();
+			}else {
+				s.push(input);
+				sum+=input;
+			}
+		}
+		br.close();
+		System.out.print(sum);
+	}
+}
---
+++ bj/src/Main_bj_9012_괄호.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_9012_괄호 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_9012.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		boolean chk;
+		String input;
+		Stack<Character> s= new Stack<>();
+		for(int n=0;n<N;n++) {
+			input=br.readLine();
+			chk=true;
+			for(int i=0;i<input.length();i++) {
+				if(input.charAt(i)=='(') {
+					s.push(input.charAt(i));
+				}else if(input.charAt(i)==')') {
+					if(s.isEmpty() || s.peek()!='(') {
+						chk=false;
+						s.clear();
+						break;
+					}else if(s.pop()==')'){
+						continue;
+					}
+				}
+				if(i==input.length()-1) {
+					if(!s.isEmpty()) {
+						chk=false;
+						s.clear();
+					}
+				}
+			}
+			if(chk) sb.append("YES").append("\n");
+			else sb.append("NO").append("\n");
+		}
+		br.close();
+		sb.setLength(sb.length()-1);
+		System.out.print(sb);
+	}
+}
