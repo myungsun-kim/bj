e0317df2d3640ac29ed15c2bf327f530bde4c46d
kms8072 <kms8072@daum.net>
2021-06-23 22:17:44 +0900

    bj 10814

---
+++ bj/src/Main_bj_s5_10814_나이순정렬.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+
+class Person{
+	int age;
+	String name;
+	
+	public Person(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+}
+
+public class Main_bj_s5_10814_나이순정렬 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10814.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Person> list = new ArrayList<>();
+		StringTokenizer st;
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			list.add(new Person(Integer.parseInt(st.nextToken()),st.nextToken()));
+		}
+		Collections.sort(list, new Comparator<Person>() {
+
+			@Override
+			public int compare(Person o1, Person o2) {
+				// TODO Auto-generated method stub
+				return Integer.compare(o1.age, o2.age);
+			}
+		});
+		
+		for(int i=0;i<n;i++) {
+			sb.append(list.get(i).age).append(" ").append(list.get(i).name).append("\n");
+		}
+		br.close();
+		sb.setLength(sb.length()-1);
+		System.out.print(sb);
+	}
+}
