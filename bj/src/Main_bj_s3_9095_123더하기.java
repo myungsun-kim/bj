b37bd7745b4092c71075475a83bb8f5a1758c344
ms <kms941110@gmail.com>
2021-07-01 23:53:55 +0900

    bj 9095

---
+++ bj/src/Main_bj_s3_9095_123더하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main_bj_s3_9095_123더하기 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_9095.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] dp;
+		for(int i=0;i<n;i++) {
+			int num = Integer.parseInt(br.readLine());
+			dp = new int[num+1];
+			dp[1]=1;
+			if(num>1) dp[2]=2;
+			if(num>2) dp[3]=4;
+			for(int j=4;j<=num;j++) {
+				dp[j]=dp[j-1]+dp[j-2]+dp[j-3];
+			}
+			sb.append(dp[num]).append('\n');
+		}
+		br.close();
+		if(n>0) sb.setLength(sb.length()-1);
+		System.out.print(sb.toString());
+	}
+}
