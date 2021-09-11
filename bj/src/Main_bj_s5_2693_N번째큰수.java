fe1575d7c7d05d9c52fa6fe90663d8ca4ce2c7bc
ms <kms941110@gmail.com>
2021-09-11 23:02:34 +0900

    bj 2693

---
+++ bj/src/Main_bj_s5_2693_N번째큰수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+// 배열 A가 주어졌을 때 N번째 큰 값을 출력하는 프로그램 작성
+// 배열 A의 크기는 항상 10, 자연수만 가지고 있다. N은 항상 3
+public class Main_bj_s5_2693_N번째큰수 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_2693.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int[] a = new int[10];
+		final int n = 3;
+		
+		for(int tc=0;tc<t;tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			for(int i=0;i<a.length;i++) {
+				a[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(a);
+			sb.append(a[a.length-n]).append('\n');
+		}
+		
+		System.out.print(sb.toString());
+	}
+}
