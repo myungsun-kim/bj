708f0faea8054c3d315d6efaae63bbbf60eccfef
ms <kms941110@gmail.com>
2021-10-14 02:37:39 +0900

    bj 1934

---
+++ bj/src/Main_bj_s5_1934_최소공배수.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+// 두 자연수의 최소 공배수 구하기
+public class Main_bj_s5_1934_최소공배수 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_1934.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		final int max = 45000; //숫자의 최대값 
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int tc=0;tc<t;tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			
+			int max_num = Math.max(num1, num2); //둘 중 더 큰 수
+			int min_num = Math.min(num1, num2); //둘 중 더 작은 수
+			
+			int answer = 0;//최소 공배수
+			int cnt = 1; //곱할 수
+
+			while(true) {
+				answer = max_num*cnt++;
+				if(answer%min_num==0) break;
+			}
+			
+			sb.append(answer).append('\n');
+		}
+		System.out.print(sb.toString());
+	}
+}
