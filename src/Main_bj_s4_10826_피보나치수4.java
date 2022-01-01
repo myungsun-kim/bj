ffe24e5304ce8bd26457f1570d50d10675203390
ms <kms941110@gmail.com>
2022-01-01 20:06:55 +0900

    fix: 백준 10826 피보나치수 4

--- src/Main_bj_s4_10826_피보나치수4.java
+++ src/Main_bj_s4_10826_피보나치수4.java
@@ -6,11 +6,17 @@ public class Main_bj_s4_10826_피보나치수4 {
 //		System.setIn(new FileInputStream("res/input_bj_10826.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		BigInteger[] dp = new BigInteger[n+1];
-		dp[1] = BigInteger.valueOf(1);
-		dp[2] = BigInteger.valueOf(1);
-		for(int i=3;i<=n;i++) dp[i]=dp[i-1].add(dp[i-2]);
-		br.close();
-		System.out.print(dp[n]);
+		if(n==0) {
+			System.out.print(0);
+		}else if(n==1){
+			System.out.print(1);			
+		}else {
+			BigInteger[] dp = new BigInteger[n+1];
+			dp[1] = BigInteger.valueOf(1);
+			dp[2] = BigInteger.valueOf(1);
+			for(int i=3;i<=n;i++) dp[i]=dp[i-1].add(dp[i-2]);
+			br.close();
+			System.out.print(dp[n]);
+		}
 	}
 }
