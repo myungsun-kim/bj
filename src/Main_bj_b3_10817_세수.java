47295c5c9368e78032ba498224c2d34c1762e7a6
ms <kms941110@gmail.com>
2022-01-05 16:21:56 +0900

    feat: 백준 10817 세 수

---
+++ src/Main_bj_b3_10817_세수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_b3_10817_세수 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10817.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final int count = 3;
+		int[] arr = new int[count];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i=0;i<arr.length;i++) arr[i] = Integer.parseInt(st.nextToken());
+		br.close();
+		Arrays.sort(arr);
+		System.out.print(arr[1]);
+	}
+}
