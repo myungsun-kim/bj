51f68534cf76180ef02aefd861b72a8bcb0ea75f
ms <kms941110@gmail.com>
2022-01-06 21:01:16 +0900

    feat: 백준 11718 그대로 출력하기

---
+++ src/Main_bj_b3_11718_그대로출력하기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_b3_11718_그대로출력하기 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_11718.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			String tmp = br.readLine();
+			if(tmp==null) break;
+			sb.append(tmp).append('\n');
+		}
+		br.close();
+		System.out.print(sb.toString());
+	}
+}
