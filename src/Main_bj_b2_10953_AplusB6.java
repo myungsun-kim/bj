6c49d976f3002084cc34c384aecf2c3b032686a4
ms <kms941110@gmail.com>
2022-01-12 16:50:39 +0900

    feat: 백준 10953 A+B-6

---
+++ src/Main_bj_b2_10953_AplusB6.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_b2_10953_AplusB6 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10953.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine()); //테스트케이스의 수
+		for(int tc=0;tc<t;tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(),",");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a+b).append('\n');
+		}
+		br.close();
+		System.out.print(sb.toString());
+	}
+}
