60eb0fd456ea32d23839d3ee631a8797ea7cb05f
ms <kms941110@gmail.com>
2022-01-13 23:12:42 +0900

    feat: 백준 10039 평균 점수

---
+++ src/Main_bj_b4_10039_평균점수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+// 점수가 40점 이상이면 그 점수를 반영. 40점 미만은 40점
+public class Main_bj_b4_10039_평균점수 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10039.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final int cnt=5; //학생의 수
+		int answer=0;
+		for(int i=0;i<cnt;i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			tmp = tmp>40?tmp:40;
+			answer+=tmp;
+		}
+		answer/=5;
+		br.close();
+		System.out.print(answer);
+	}
+}
