11b9d39cc9ba0254b9a6ab7f452828fe38d568c4
ms <kms941110@gmail.com>
2022-01-15 19:36:06 +0900

    feat: 백준 10808 알파벳 개수

---
+++ src/Main_bj_b2_10808_알파벳개수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main_bj_b2_10808_알파벳개수 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10808.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String input = br.readLine();
+		final int size = 26;
+		int[] count = new int[size];
+		for(int i=0;i<input.length();i++) count[input.charAt(i)-'0'-49]++;
+		for(int i=0;i<count.length;i++) sb.append(count[i]).append(' ');
+		br.close();
+		System.out.print(sb.toString());
+	}
+}
