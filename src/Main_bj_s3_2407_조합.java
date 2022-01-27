5f3d01b742e36fcf1d56e4cd0d613d1c79a96135
ms <kms941110@gmail.com>
2022-01-27 22:04:24 +0900

    feat: 백준 2407 조합

---
+++ src/Main_bj_s3_2407_조합.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+// nCm 출력
+public class Main_bj_s3_2407_조합 {
+	public static void main(String[] args) throws Exception{
+//		System.setIn(new FileInputStream("res/input_bj_2407.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		long n = Long.parseLong(st.nextToken());
+		long m = Long.parseLong(st.nextToken());
+		BigInteger answer = BigInteger.ONE;
+		for(long i=n;i>n-m;--i) {
+			answer = answer.multiply(BigInteger.valueOf(i));
+		}
+		for(long i=2;i<=m;++i) {
+			answer = answer.divide(BigInteger.valueOf(i));
+		}
+		br.close();
+		System.out.print(answer);
+	}
+}
