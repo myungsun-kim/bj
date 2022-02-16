36e36cb8ac4ac7cf0019809a2731fc611c920d8e
ms <kms941110@gmail.com>
2022-02-16 22:44:17 +0900

    feat: 백준 10829 이진수 변환

---
+++ src/Main_bj_b2_10829_이진수변환.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main_bj_b2_10829_이진수변환 {
+	static StringBuilder sb = new StringBuilder();
+	static BigInteger two = new BigInteger("2");
+	static BigInteger zero = new BigInteger("0");
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10829.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BigInteger n = new BigInteger(br.readLine());
+		binary(n);
+		br.close();
+		System.out.print(sb.reverse().toString());
+	}
+	
+	static void binary(BigInteger n) {
+		if(n.equals(zero)) return;
+		sb.append(n.mod(two));
+		binary(n.divide(two));
+	}
+}
