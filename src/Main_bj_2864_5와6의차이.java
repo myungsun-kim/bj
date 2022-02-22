7728334bee296a9b102e3c624c2e73a8d54d01aa
ms <kms941110@gmail.com>
2022-02-23 00:46:00 +0900

    rename

--- src/Main_bj_2864_5와6의차이.java
+++ src/Main_bj_2864_5와6의차이.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 // 5를 6으로, 6을 5로 잘못 볼 수 있음
 // 두 수가 주어졌을 때 두 수의 가능한 합 중 최솟값과 최댓값을 출력
-public class Main_bj_b2_2864_5와6의차이 {
+public class Main_bj_2864_5와6의차이 {
 	public static void main(String[] args) throws Exception{
 //		System.setIn(new FileInputStream("res/input_bj_2864.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
