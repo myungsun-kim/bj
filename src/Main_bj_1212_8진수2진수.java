b758de3c008239e0805de08b66d8f21728ef34a4
ms <kms941110@gmail.com>
2022-03-08 23:50:26 +0900

    rename

--- src/Main_bj_1212_8진수2진수.java
+++ src/Main_bj_1212_8진수2진수.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 // 8진수가 주어졌을 때, 2진수로 변환하는 프로그램 작성
 // 각 자리수를 이진변환 후(단, 세자리) 합치면 2진수가 됨
-public class Main_bj_b3_1212_8진수2진수 {
+public class Main_bj_1212_8진수2진수 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_1212.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
