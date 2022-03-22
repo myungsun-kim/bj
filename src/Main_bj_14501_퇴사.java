4a6dd5f139901edf20ff3f70d50da0103fcadd01
ms <kms941110@gmail.com>
2022-03-22 23:47:24 +0900

    refactor: bj 14501

--- src/Main_bj_14501_퇴사.java
+++ src/Main_bj_14501_퇴사.java
@@ -5,11 +5,10 @@ import java.util.*;
 // 각각의 상담은 상담을 완료하는데 걸리는 시간 t[i]와 받을 수 있는 금액 p[i]로 이루어져있다
 // 상담을 하는데 필요한 기간은 1일보다 클 수 있기에 모든 상담을 할 수는 없다
 // n일 동안 상담을 할 때 얻을 수 있는 최대 수익
-public class Main_bj_s4_14501_퇴사 {
+public class Main_bj_14501_퇴사 {
 	public static void main(String[] args) throws Exception{
 //		System.setIn(new FileInputStream("res/input_bj_14501.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());//상담 개수
 		int[][] counsel = new int[n+1][2]; //상담 정보
 		int[] dp = new int[n+1]; //해당 날짜까지의 최대 수익 저장
