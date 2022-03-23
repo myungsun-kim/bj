1b84121121f7596305cc7d8c5fdf5355fdb1f910
ms <kms941110@gmail.com>
2022-03-23 22:33:27 +0900

    rename

--- src/Main_bj_9012_괄호.java
+++ src/Main_bj_9012_괄호.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main_bj_s4_9012_괄호 {
+public class Main_bj_9012_괄호 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_9012.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
