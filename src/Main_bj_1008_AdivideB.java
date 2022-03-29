9af3983c9dd2a52e56036ac82da4c102f38277c9
ms <kms941110@gmail.com>
2022-03-29 23:38:15 +0900

    rename

--- src/Main_bj_1008_AdivideB.java
+++ src/Main_bj_1008_AdivideB.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main_bj_1008_AdivisionB {
+public class Main_bj_1008_AdivideB {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double a = sc.nextInt();
