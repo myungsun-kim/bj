fbe1265eb7faf37d58c270ecac774f7a0dc738e3
ms <kms941110@gmail.com>
2022-04-06 23:25:04 +0900

    refactor: 백준 9095 123더하기

--- src/Main_bj_9095_123더하기.java
+++ src/Main_bj_9095_123더하기.java
@@ -1,6 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
-public class Main_bj_s3_9095_123더하기 {
+public class Main_bj_9095_123더하기 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_9095.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
