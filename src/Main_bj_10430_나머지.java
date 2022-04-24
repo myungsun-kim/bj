e617d7705e896b6857394be05501f2b85ab84b36
ms <kms941110@gmail.com>
2022-04-24 23:19:40 +0900

    refactor: 백준 10430 나머지

--- src/Main_bj_10430_나머지.java
+++ src/Main_bj_10430_나머지.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-public class Main_bj_b5_10430_나머지 {
+public class Main_bj_10430_나머지 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
+		sc.close();
 		
 		System.out.println((a+b)%c);
 		System.out.println(((a%c)+(b%c))%c);
