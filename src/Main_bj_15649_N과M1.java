729fa9eace80da8862250b3c5d8d1bf1eae2ef50
ms <kms941110@gmail.com>
2022-04-29 01:19:40 +0900

    refactor: 백준 N과 M1

--- src/Main_bj_15649_N과M1.java
+++ src/Main_bj_15649_N과M1.java
@@ -1,5 +1,7 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_bj_15649_N과M1 {
 	static int n, m;
