fd1ad8a465837de815b2428153f7b1421bdb468a
ms <kms941110@gmail.com>
2022-05-03 00:03:45 +0900

    refactor: 백준 11654 아스키코드

--- src/Main_bj_11654_아스키코드.java
+++ src/Main_bj_11654_아스키코드.java
@@ -1,8 +1,10 @@
-import java.io.*;
-import java.util.*;
-public class Main_bj_b5_11654_아스키코드 {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Main_bj_11654_아스키코드 {
 	public static void main(String[] args) throws Exception{
-//		System.setIn(new FileInputStream("res/input_bj_11654.txt"));
+		System.setIn(new FileInputStream("res/input_bj_11654.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char input = br.readLine().charAt(0);
 		
