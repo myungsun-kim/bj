a7909f9fa359467c435f5a5e76789d78310f0c62
ms <kms941110@gmail.com>
2022-05-14 00:10:34 +0900

    refactor: 백준 5532 방학숙제

--- src/Main_bj_5532_방학숙제.java
+++ src/Main_bj_5532_방학숙제.java
@@ -1,10 +1,11 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // 방학 숙제는 수학과 국어 문제 풀기
 // 방학은 총 L일, 국어는 총 A페이지, 수학은 총 B페이지
 // 상근이는 하루에  국어 최대 C페이지, 수학 최대 D페이지를 풀 수 있음
 // 상근이가 겨울 방학동안 숙제를 하지 않고 놀 수 있는 최대 날의 수 구하기
-public class Main_bj_b4_5532_방학숙제 {
+public class Main_bj_5532_방학숙제 {
 	public static void main(String[] args) throws Exception{
 //		System.setIn(new FileInputStream("res/input_bj_5532.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
