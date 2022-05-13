d5785cb88115ad1d37353eb08d1b41dfeba887cf
ms <kms941110@gmail.com>
2022-05-13 23:55:58 +0900

    refactor: 백준 2420 사파리 월드

--- src/Main_bj_2420_사파리월드.java
+++ src/Main_bj_2420_사파리월드.java
@@ -1,7 +1,8 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main_bj_b4_2420_사파리월드 {
+public class Main_bj_2420_사파리월드 {
 	public static void main(String[] args) throws Exception{
 //		System.setIn(new FileInputStream("res/input_bj_2420.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
