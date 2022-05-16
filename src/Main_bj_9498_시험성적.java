939782e096c9382219c20c78c406f7992e18e73a
ms <kms941110@gmail.com>
2022-05-16 23:26:36 +0900

    refactor: 백준 9498 시험 성적

--- src/Main_bj_9498_시험성적.java
+++ src/Main_bj_9498_시험성적.java
@@ -1,21 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Main_bj_b4_9498_시험성적 {
+public class Main_bj_9498_시험성적 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int j = Integer.parseInt(br.readLine());
 		if(j>=90) {
-			sb.append("A");
+			sb.append('A');
 		}else if(j>=80) {
-			sb.append("B");
+			sb.append('B');
 		}else if(j>=70) {
-			sb.append("C");
+			sb.append('C');
 		}else if(j>=60) {
-			sb.append("D");
+			sb.append('D');
 		}else {
-			sb.append("F");
+			sb.append('F');
 		}
 		br.close();
 		System.out.print(sb);
