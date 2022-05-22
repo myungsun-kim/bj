cca941d7d8f83a52ae583f147b911a8337a9c744
ms <kms941110@gmail.com>
2022-05-22 16:24:24 +0900

    refactor: 백준 2609 최대 공약수와 최소 공배수

--- src/Main_bj_2609_최대공약수와최소공배수.java
+++ src/Main_bj_2609_최대공약수와최소공배수.java
@@ -1,29 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main_bj_s5_2609_최대공약수와최소공배수 {
+public class Main_bj_2609_최대공약수와최소공배수 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_2609.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		StringBuilder sb = new StringBuilder();
-		int N1=Integer.parseInt(st.nextToken());
-		int N2=Integer.parseInt(st.nextToken());
-		for(int i=Math.min(N1, N2);i>0;i--) {
-			if(N1%i==0 && N2%i==0) {
+		int n1=Integer.parseInt(st.nextToken());
+		int n2=Integer.parseInt(st.nextToken());
+		for(int i=Math.min(n1, n2);i>0;i--) {
+			if(n1%i==0 && n2%i==0) {
 				sb.append(i).append("\n");//최소 공약수
 				break;
 			}
 		}
-		int num=Math.max(N1, N2);
+		int num=Math.max(n1, n2);
 		while(true) {//최대 공배수
-			if(num%N1==0 && num%N2==0) {
+			if(num%n1==0 && num%n2==0) {
 				sb.append(num);
 				break;
 			}
 			num++;
 		}
 		br.close();
-		System.out.print(sb);
+		System.out.print(sb.toString());
 	}
 }
