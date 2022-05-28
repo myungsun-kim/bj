e875eb97bc973d01e4395de753580fd5557020b9
ms <kms941110@gmail.com>
2022-05-28 23:59:34 +0900

    refactor: 백준 7568 덩치

--- src/Main_bj_7568_덩치.java
+++ src/Main_bj_7568_덩치.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main_bj_s5_7568_덩치 {
+public class Main_bj_7568_덩치 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_7568.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,7 +11,7 @@ public class Main_bj_s5_7568_덩치 {
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
 		int[][] arr = new int[n][2];//사람들의 키와 몸무게를 저장
-		int grade;
+		
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
 			arr[i][0] = Integer.parseInt(st.nextToken());//몸무게
@@ -17,7 +19,7 @@ public class Main_bj_s5_7568_덩치 {
 		}
 		
 		for(int i=0;i<n;i++) {
-			grade = 1;
+			int grade = 1;
 			for(int j=0;j<n;j++) {				
 				if(arr[i][0]<arr[j][0] && arr[i][1]<arr[j][1]) grade++;
 			}
