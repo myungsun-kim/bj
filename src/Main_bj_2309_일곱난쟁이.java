338c6b4fc36dcaa2f02f143e777d1f26b7a63521
ms <kms941110@gmail.com>
2022-05-31 00:32:34 +0900

    refactor: 백준 2309 일곱난쟁이

--- src/Main_bj_2309_일곱난쟁이.java
+++ src/Main_bj_2309_일곱난쟁이.java
@@ -1,20 +1,22 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 // 아홉명의 난쟁이 중 일곱난쟁이를 찾는다
 // 일곱난쟁이의 키의 합은 100
 // 아홉명의 난쟁이의 키의 합-일곱명의 난쟁이가 아닌 난쟁이 두명의 키의 합=100
 public class Main_bj_2309_일곱난쟁이 {
-	
+	static StringBuilder sb = new StringBuilder();
+	static int[] arr;
+	static final int count = 9;
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_2309.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		final int total=9;
-		int[] arr = new int[total];
+		arr = new int[count];
 		int sum = 0;
 		
-		for(int i=0;i<total;i++) {
+		for(int i=0;i<count;i++) {
 			arr[i]=Integer.parseInt(br.readLine());
 			sum+=arr[i];
 		}
@@ -22,15 +24,11 @@ public class Main_bj_2309_일곱난쟁이 {
 		
 		boolean check=false;//일곱난쟁이가 아닌 난쟁이들을 찾았는지
 		
-		for(int i=0;i<total;i++) {//일곱난쟁이가 아닌 난쟁이 둘을 찾는다
-			for(int j=0;j<total;j++) {
-				if(i!=j && arr[i]+arr[j]==sum-100) {
+		for(int i=0;i<count;i++) {//일곱난쟁이가 아닌 난쟁이 둘을 찾는다
+			for(int j=i+1;j<count;j++) {
+				if(sum-arr[i]-arr[j]==100) {
+					print(i,j);
 					check=true;
-				}
-				if(check) {
-					for(int k=0;k<total;k++) {
-						if(k!=i && k!=j) sb.append(arr[k]).append('\n');
-					}
 					break;
 				}
 			}
@@ -40,4 +38,10 @@ public class Main_bj_2309_일곱난쟁이 {
 		System.out.print(sb.toString());
 	}
 	
+	static void print(int idx1, int idx2) {
+		for(int i=0;i<count;i++) {
+			if(i==idx1 || i==idx2) continue;
+			sb.append(arr[i]).append('\n');
+		}
+	}
 }
