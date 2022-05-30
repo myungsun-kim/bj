d6dc5ceb14f94094bcb2b2ee9f9623ff37968eb8
ms <kms941110@gmail.com>
2022-05-31 00:09:38 +0900

    refactor: 백준 10430 나머지

--- src/Main_bj_10430_나머지.java
+++ src/Main_bj_10430_나머지.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_bj_10430_나머지 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		sc.close();
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_10430.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		
-		System.out.println((a+b)%c);
-		System.out.println(((a%c)+(b%c))%c);
-		System.out.println((a*b)%c);
-		System.out.println(((a%c)*(b%c))%c);
+		sb.append((a+b)%c).append('\n');
+		sb.append(((a%c)+(b%c))%c).append('\n');
+		sb.append((a*b)%c).append('\n');
+		sb.append(((a%c)*(b%c))%c);
+		br.close();
+		System.out.print(sb.toString());
 	}
 }
