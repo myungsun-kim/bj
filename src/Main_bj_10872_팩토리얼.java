0510881c74e4693be63d0b87bcf02cd6d556f805
ms <kms941110@gmail.com>
2022-05-31 00:17:48 +0900

    refactor: 백준 10872 팩토리얼

--- src/Main_bj_10872_팩토리얼.java
+++ src/Main_bj_10872_팩토리얼.java
@@ -1,11 +1,13 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Main_bj_10872_팩토리얼 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_10872.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		br.close();
 		System.out.print(factorial(n));
 	}
 
