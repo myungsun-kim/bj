a1ee013e81edf4621c84a0c4744766a12a8983d5
ms <kms941110@gmail.com>
2022-06-01 23:46:10 +0900

    refactor: 백준 5568 카드 놓기

--- src/Main_bj_5568_카드놓기.java
+++ src/Main_bj_5568_카드놓기.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Main_bj_s5_5568_카드놓기 {
+public class Main_bj_5568_카드놓기 {
 	static int n,k;
 	static int[] card, selected;
 	static boolean[] visited;
