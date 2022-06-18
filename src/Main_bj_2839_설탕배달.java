266fd8c2d0b8d322f87a979205f57c3c2b418a65
ms <kms941110@gmail.com>
2022-06-19 01:00:11 +0900

    refactor: 백준 2839 설탕배달

--- src/Main_bj_2839_설탕배달.java
+++ src/Main_bj_2839_설탕배달.java
@@ -2,29 +2,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main_bj_2839_설탕배달 {
-	static int N;
-	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
+		int n= Integer.parseInt(br.readLine());
 		int cnt = -1;//총 봉지의 개수
-		int min = N/5;
+		int min = n/5;
 		
-		if(N == 5*min) {
+		if(n == 5*min) {
 			cnt = min;
 		}else {
 			while(min>=0) {
-				if((N-5*min)%3 == 0) {
-					cnt = (N-5*min)/3+min;
+				if((n-5*min)%3 == 0) {
+					cnt = (n-5*min)/3+min;
 					break;
 				}
 				min--;
 			}
 		}
-		sb.append(cnt);
-		System.out.print(sb);
+		br.close();
+		System.out.print(cnt);
 	}
-
-
 }
