3ae3cf5e119e4e207474c68ec92488bc0a78a2ec
ms <kms941110@gmail.com>
2022-06-23 23:59:49 +0900

    refactor: 백준 2578 빙고

--- src/Main_bj_2578_빙고.java
+++ src/Main_bj_2578_빙고.java
@@ -1,8 +1,10 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // 사회자가 몇 번째 수를 부른 후 빙고를 외치게 되는지 출력
 // 선이 세 개 이상 그어지면 빙고
-public class Main_bj_s5_2578_빙고 {
+public class Main_bj_2578_빙고 {
 	static int[][] arr;
 	static final int size = 5;
 	static int rows;
