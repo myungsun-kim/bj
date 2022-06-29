ce84516ca337a7130e10d5aba45e35c989f488f2
ms <kms941110@gmail.com>
2022-06-30 00:57:50 +0900

    refactor: 백준 2693 N번째 큰 수

--- src/Main_bj_2693_N번째큰수.java
+++ src/Main_bj_2693_N번째큰수.java
@@ -1,8 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 // 배열 A가 주어졌을 때 N번째 큰 값을 출력하는 프로그램 작성
 // 배열 A의 크기는 항상 10, 자연수만 가지고 있다. N은 항상 3
-public class Main_bj_s5_2693_N번째큰수 {
+public class Main_bj_2693_N번째큰수 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_2693.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
