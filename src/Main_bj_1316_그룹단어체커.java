6e99aa6341938481630cd297b164a64f158e610a
myungsun-kim <74165983+myungsun-kim@users.noreply.github.com>
2023-05-26 09:39:57 +0900

    백준 1316 그룹단어체커

--- src/Main_bj_1316_그룹단어체커.java
+++ src/Main_bj_1316_그룹단어체커.java
@@ -6,22 +6,25 @@ public class Main_bj_1316_그룹단어체커 {
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("res/input_bj_1316.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int cnt=0;//그룹단어의 개수
-		String word;
-		for(int i=0;i<n;i++) {
-			boolean[] chk = new boolean[26];
-			word = br.readLine();
-			for(int j=0;j<word.length();j++) {
-				if(chk[word.charAt(j)-97]) break; //이전에 나온 적이 있음
-				if(j+1<word.length() && word.charAt(j)!=word.charAt(j+1)) {
-					chk[word.charAt(j)-97]=true;
-					continue;
-				}
-				if(j==word.length()-1) cnt++;
-			}
-		}
-		br.close();
-		System.out.print(cnt);
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < n; ++i) {
+            char[] input = br.readLine().toCharArray();
+            boolean[] visited = new boolean[26]; // 이전에 나온 문자인지 아닌지
+            boolean check = true; // 그룹문자인지 아닌지
+            for (int j = 0; j < input.length; ++j) {
+                int cur = input[j] - 'a';
+                if (j != 0 && visited[cur] && input[j - 1] != input[j]) {
+                    check = false;
+                    break;
+                } else {
+                    visited[cur] = true;
+                }
+            }
+            if (check)
+                ++count;
+        }
+        System.out.print(count);
 	}
 }
